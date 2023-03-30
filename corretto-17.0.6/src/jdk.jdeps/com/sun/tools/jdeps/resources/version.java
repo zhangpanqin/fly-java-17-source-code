e30fcf62ac9ecec96a93ff13e7d0f52bb32c902c
zhangpanqin <zhangpanqin@outlook.com>
2023-03-30 13:35:16 +0800

    环境搭建

---
+++ corretto-17.0.6/src/jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "17.0.6+10-LTS" },
+            { "jdk", "17.0.6" },
+            { "release", "17.0.6" },
+        };
+    }
+}
---
+++ src.main.java/com/mflyyou/java17/Demo.java
@@ -0,0 +1,7 @@
+package com.mflyyou.java17;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(new String("1112"));
+    }
+}
